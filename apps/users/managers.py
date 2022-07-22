e18712337ead02b0ac19822fb8e35532e953966f
kad1m <dmytro.lytvynenko@icloud.com>
2022-07-22 13:29:21 +0300

    configure pytest and coverage

--- apps/users/managers.py
+++ apps/users/managers.py
@@ -52,7 +52,7 @@ class CustomUserManager(BaseUserManager):
         extra_fields.setdefault("is_active", True)
 
         if extra_fields.get("is_staff") is not True:
-            raise ValueError(_("Superusers must have is_stuf=True"))
+            raise ValueError(_("Superusers must have is_staff=True"))
 
         if extra_fields.get("is_superuser") is not True:
             raise ValueError(_("Superusers must have is_superuser=True"))
