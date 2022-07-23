425981ce636d229095f883d9e81eef1b3b952275
kad1m <dmytro.lytvynenko@icloud.com>
2022-07-23 21:12:11 +0300

    change docstring

--- tests/profiles/test_models.py
+++ tests/profiles/test_models.py
@@ -2,5 +2,5 @@ import pytest
 
 
 def test_profile_str(profile):
-    """Test profile str representation"""
+    """Test the profile model string representation"""
     assert profile.__str__() == f"{profile.user.username}'s profile"
