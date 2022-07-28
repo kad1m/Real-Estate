ecc8a71348ca6b11d563825872e79496194932fb
kad1m <dmytro.lytvynenko@icloud.com>
2022-07-29 02:05:09 +0300

    setup React with docker

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './App.css';
+
+function App() {
+  return (
+    <div >
+     <h1>Real estate</h1>
+      <p>
+        the real test starts here...
+      </p>
+    </div>
+  );
+}
+
+export default App;
