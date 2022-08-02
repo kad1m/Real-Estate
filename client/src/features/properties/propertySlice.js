962f28bde2256bfe6ada7a670c10664c2d3c6633
kad1m <dmytro.lytvynenko@icloud.com>
2022-08-03 01:50:32 +0300

    add property component

--- client/src/features/properties/propertySlice.js
+++ client/src/features/properties/propertySlice.js
@@ -1,51 +1,57 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
-import propertyAPIService from './propertyAPIService'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import propertyAPIService from "./propertyAPIService";
 
 const initialState = {
-    properties: [],
-    property: {},
-    isError: false,
-    isLoading: false,
-    isSuccess: false,
-    message: ''
-}
+	properties: [],
+	property: {},
+	isError: false,
+	isLoading: false,
+	isSuccess: false,
+	message: "",
+};
 
-export const getProperties = createAsyncThunk('properties/getAll', async(_, thunkAPI) =>{
-try {
-    return await propertyAPIService.getProperties()
-} catch (error) {
-    const message = (error.response && error.response.data && error.response.data.message) || error.message || error.toString()
+// get all properties
+export const getProperties = createAsyncThunk(
+	"properties/getAll",
+	async (_, thunkAPI) => {
+		try {
+			return await propertyAPIService.getProperties();
+		} catch (error) {
+			const message =
+				(error.response &&
+					error.response.data &&
+					error.response.data.message) ||
+				error.message ||
+				error.toString();
 
-    return thunkAPI.rejectWithValue(
-        message
-    )
-}
-})
+			return thunkAPI.rejectWithValue(message);
+		}
+	}
+);
 
 export const propertySlice = createSlice({
-    name: 'property',
-    initialState,
-    reducers: {
-        reset: (state) => initialState
-    },
-    extraReducers: (builder) => {
-        builder
-            .addCase(getProperties.pending, (state)=>{
-                state.isLoading = true
+	name: "property",
+	initialState,
+	reducers: {
+		reset: (state) => initialState,
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(getProperties.pending, (state) => {
+				state.isLoading = true;
+			})
+			.addCase(getProperties.fulfilled, (state, action) => {
+				state.isLoading = false;
+				state.isSuccess = true;
+				state.properties = action.payload.results;
+			})
+			.addCase(getProperties.rejected, (state, action) => {
+				state.isLoading = false;
+				state.isError = true;
+				state.message = action.payload;
+			});
+	},
+});
 
-            })
-            .addCase(getProperties.fulfilled, (state, action) =>{
-                state.isLoading = false
-                state.isSuccess = true
-                state.properies = action.payload
-            })
-            .addCase(getProperties.rejected, (state, action)=>{
-                state.isLoading = false
-                state.isError = true
-                state.properies = action.payload
-            })
-    }
-})
-
-export const {reset} = propertySlice.actions
-export default propertySlice.reducer
+export const { reset } = propertySlice.actions;
+export default propertySlice.reducer;
